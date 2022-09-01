1305179de5fafc537eb134054673da1aa56e1c49
Dedeco1225 <andrericardofreitas3@hotmail.com>
2022-09-01 16:40:17 -0300

    Communit2

--- game.py
+++ game.py
@@ -1,4 +1,30 @@
-print('hello dica')
+# Frist Game Ping-Pong
+
+# baixando Interface tkinter
+from logging import root
+from operator import length_hint
+from tkinter import *
+import random
+# function
+import time
+
+level = int(input("Qual o nível você gostaria de jogar? 1/2/3/4/5 \n"))
+length = 502/level
+
+# Janela raiz na qual todos os outros widgets vão. É uma instância da classe Tk.
+root = Tk()
+# funções Built-in de Strings
+root.title("Ping Pong")
+root.resizable(0,0)
+root.wm_attributes("-topmost", -1)
+
+
+
+
+
+
+
+
 
 
 
