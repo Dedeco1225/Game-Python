0c3a01edbf7b7480ee6a47796b00ed0d230d4d17
Dedeco1225 <andrericardofreitas3@hotmail.com>
2022-09-11 13:53:34 -0300

    Commint4

--- game.py
+++ game.py
@@ -1,11 +1,13 @@
 # Frist Game Ping-Pong
 
 # baixando Interface tkinter
+from __future__ import barry_as_FLUFL
 from asyncio import start_server
 from itertools import count
 from logging import root
 from multiprocessing.util import log_to_stderr
-from operator import length_hint
+from operator import length_hint, truediv
+from ossaudiodev import SNDCTL_COPR_RESET
 from textwrap import fill
 from tkinter import *
 import random
@@ -14,6 +16,7 @@ import time
 from tracemalloc import start
 from turtle import pos
 from typing_extensions import Self
+from xml.etree.ElementInclude import DEFAULT_MAX_INCLUSION_DEPTH
 
 level = int(input("Qual o nível você gostaria de jogar? 1/2/3/4/5 \n"))
 length = 502/level
@@ -64,6 +67,102 @@ class Bola:
             self.x = -3
 
         self.Barra_pos = self.canvas.coords(self.Barra.id)
+
+        if pos[2] >= self.Barra_pos[0] and pos[0] <= self.Barra_pos[2]:
+            if pos[3] >= self.Barra_pos[1] and pos[3] <= self.Barra_pos[3]:
+                self.y = -3
+                global count
+                count +=1
+                score()
+
+        if pos[3] <= self.canvas_height:
+            self.canvas.after(10, self.draw)
+        
+        else:
+            game_over()
+            global lost
+            lost = True
+
+class Barra:
+    def __init__(self, canvas, color):
+        self.canvas = canvas
+        self.id = canvas.create_rectangle(0, 0, length, 10, fill=color)
+        self.canvas.move(self, id, 200, 400)
+
+        self.x = 0
+
+        self.canvas_width = self.canvas.winfo_width()
+
+        self.canvas.bind_all("<Keypress-Left>", self.move_left)
+        self.canvas.bind_all("<Keypress-Left>", self.move_right)
+
+    def draw(self):
+        self.canvas.move(self.id, self.x, 0)
+
+        self.pos = self.canvas.coords(self.id)
+
+        if self.pos[0] <= 0:
+            self.x = 0
+
+        if self.pos[2] >= self.canvas_width:
+            self.x = 0
+
+        global lost
+
+        if lost == False:
+            self.canvas.after(10, self.draw)
+
+    def move_left(self, event):
+        if self.pos[0] >= 0:
+            self.x = -3
+
+    def move_right(self, event):
+        if self.pos[2] <= self.canvas_width:
+            self.x = 3
+
+    def start_game(event):
+    global lost, count
+    lost = False
+    count = 0
+    score()
+    canvas.itemconfig(game, text=" ")
+
+    time.sleep(1)
+    Barra.draw()
+    Bola.draw()
+
+
+def score():
+    canvas.itemconfig(score_now, text="Pontos: " + str(count))
+
+def game_over():
+    canvas.itemconfig(game, text="Game over!")
+
+
+Barra = Barra(canvas, "orange")
+Bola = Bola(canvas, Barra, "black")
+
+
+score_now = canvas.create_text(430, 20, text="Pontos: " + str(count), fill = "green", font=("Arial", 16))
+game = canvas.create_text(400, 300, text=" ", fill="red", font=("Arial", 40))
+
+
+canvas.bind_all("<Button-1>", start_game)
+
+root.mainloop()        
+    
+
+
+
+
+
+
+
+
+
+
+
+
 
         
 
